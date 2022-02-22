4d128040147f4167eec2d0d12ca401498e0a2a76
Johannes Rave <johannes.rave@innoq.com>
2022-02-23 00:00:34 +0100

    refactoring token styles into element inline

--- src/ts/gameToken.ts
+++ src/ts/gameToken.ts
@@ -1,47 +1,55 @@
 export class GameToken extends HTMLElement {
   value: number = 2;
   id!: string;
+  init = true;
+
   private oldOffsetTop!: number;
   private oldOffsetLeft!: number;
 
   connectedCallback() {
-    this.id = (this.id === '') ? "id" + Math.random().toString(16).slice(2, 10) : this.id;
-    this.setAttribute('id', this.id);
+    if (this.init) {
+      this.id = "id" + Math.random().toString(16).slice(2, 10);
+      this.setAttribute('id', this.id);
+      this.init = false;
 
-    this.oldOffsetLeft = this.oldOffsetLeft ? this.oldOffsetLeft : this.offsetLeft;
-    this.oldOffsetTop = this.oldOffsetTop ? this.oldOffsetTop : this.offsetTop;
+      this.style.contain = 'content';
+      this.style.display = 'grid';
+      this.style.placeContent = 'center';
+      this.style.placeItems = 'center';
+      this.style.position = 'relative';
+      // this.style.animation = `slide${this.id} 100ms ease-in-out`;
+    }
+
+    // this.oldOffsetLeft = this.oldOffsetLeft || this.offsetLeft;
+    // this.oldOffsetTop = this.oldOffsetTop || this.offsetTop;
+    this.oldOffsetLeft = this.oldOffsetLeft ?? this.offsetLeft;
+    this.oldOffsetTop = this.oldOffsetTop ?? this.offsetTop;
 
     this.innerHTML = `
     <style>
-      game-token {
-        display: grid;
-        place-content: center;
-        place-items: center;
-        contain: content;
-        position: relative;
-      }
-
       game-token#${this.id} {
-        animation: slide${this.id} 250ms ease-in-out;
+        animation: slide${this.id} 1000ms ease-in-out;
       }
 
       @keyframes slide${this.id} {
         from{
-            left: ${this.oldOffsetLeft - this.offsetLeft}px;
-            top: ${this.oldOffsetTop - this.offsetTop}px;
+          transform:
+            translateX(${this.oldOffsetLeft - this.offsetLeft})
+            translateY(${this.oldOffsetTop - this.offsetTop});
         }
         to {
-            left: 0;
-            top: 0;
+          transform:
+            translateX(0)
+            translateY(0);
         }
       }
     </style>
     <p>${this.value}</p>
     `
 
-    // console.info(`Token was at ${this.oldOffsetLeft}px / ${this.oldOffsetTop}px`)
-    // console.info(`Connecting Token at ${this.offsetLeft}px / ${this.offsetTop}px`)
-    this.oldOffsetTop = this.offsetTop;
+    console.info(`Token was at ${this.oldOffsetLeft}px / ${this.oldOffsetTop}px`)
+    console.info(`Connecting Token at ${this.offsetLeft}px / ${this.offsetTop}px`)
     this.oldOffsetLeft = this.offsetLeft;
+    this.oldOffsetTop = this.offsetTop;
   }
 }
